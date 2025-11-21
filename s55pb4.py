dab63b5d8a724393a2104cfd71a29b9d41fc2209
Naveen A <2006naveen18@gmail.com>
2025-11-21 13:59:52 +0530

    Add files via upload

---
+++ s55pb4.py
@@ -0,0 +1,6 @@
+#to find whether a given username contains less than 10 characters or not
+username = input("enter the username:")
+if len(username)<10:
+    print("username is valid")  
+else:
+    print("username is invalid")
