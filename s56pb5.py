b4a528ceb74672fa59ef01b7ba57aa3a38edbdaf
Naveen A <2006naveen18@gmail.com>
2025-11-24 22:14:05 +0530

    Add files via upload

---
+++ s56pb5.py
@@ -0,0 +1,6 @@
+# to find out whether a given post is taking about "yogesh" or not 
+post = input("enter the name: ")
+if "yogesh" in post:
+      print("This post is talking about Yogesh.")
+else:
+     print("this is not taken by yogesh") 
