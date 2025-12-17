b9376f69c424e5e91f5d94d98037cb71fdac3861
Naveen A <2006naveen18@gmail.com>
2025-12-17 19:13:52 +0530

    Fix indentation and clean up s23.py

--- s23.py
+++ s23.py
@@ -3,9 +3,7 @@ import pyttsx3
 engine = pyttsx3.init()
 engine.say("hey i am naveen in acharya college of engineering")
 engine.runAndWait()
-
-
-
 st = "this is acharya"
 print(type(st))
 print(st)
+
